31ccc6beee476d8df1616436ecda2a1c7af18b87
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-04-03 20:52:47 +0530

    Time: 37 ms (85.80%), Space: 16.7 MB (95.49%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,4 +1,47 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return [*map(s.index, s)] == [*map(t.index, t)]
+        if len(s) != len(t):
+            return False
+        
+        s_to_t = {}  # Dictionary to store mappings from s to t
+        t_to_s = {}  # Dictionary to store mappings from t to s
+        
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]
+            
+            # Check if the current mapping violates any condition
+            if (char_s in s_to_t and s_to_t[char_s] != char_t) or \
+               (char_t in t_to_s and t_to_s[char_t] != char_s):
+                return False
+            
+            # Update mappings
+            s_to_t[char_s] = char_t
+            t_to_s[char_t] = char_s
+        
+        return True
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # return [*map(s.index, s)] == [*map(t.index, t)]
         
