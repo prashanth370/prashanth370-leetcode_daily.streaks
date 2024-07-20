0dea7d5d21a3069596623112902ae3c2bd62782a
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-07-20 22:35:22 +0530

    Time: 360 ms (95.67%), Space: 22 MB (14.08%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -0,0 +1,21 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        col_sum = colSum
+        row_sum = rowSum
+        
+        mat = [[0]*len(col_sum) for i in range(len(row_sum))]
+        i = 0
+        j = 0
+        while i < len(row_sum) and j < len(col_sum):
+            mat[i][j] = min(row_sum[i], col_sum[j])
+            if row_sum[i] == col_sum[j]:
+                i += 1
+                j += 1
+            elif row_sum[i] > col_sum[j]:
+                row_sum[i] -= col_sum[j]
+                j += 1
+            else:
+                col_sum[j] -= row_sum[i]
+                i += 1
+
+        return mat
