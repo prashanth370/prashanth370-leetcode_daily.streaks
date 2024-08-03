03d1dea953d5a1d0a95c56325114a90ad75e5abb
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-08-03 10:24:03 +0530

    Time: 82 ms (16.17%), Space: 16.8 MB (59.96%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
@@ -4,8 +4,8 @@ class Solution:
         # return sorted(target) == sorted(arr)
 
 
-        val = sorted(arr)
-        k = sorted(target)
+        # val = sorted(arr)
+        # k = sorted(target)
 
         if sorted(target) == sorted(arr):
             return "true"
