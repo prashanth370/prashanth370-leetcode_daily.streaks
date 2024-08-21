08456eea3cb3f5083db3e03046129fee78e115d5
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-08-21 23:45:38 +0530

    Time: 333 ms (61.54%), Space: 16.6 MB (95.24%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.py
@@ -0,0 +1,36 @@
+# class Solution:
+#     def strangePrinter(self, s: str) -> int:
+#         n = len(s)
+#         if n==0:
+#             return 0
+#         count = 0
+#         i = 0
+#         while i<n:
+#             j = i
+#             while j<n and s[j] == s[i]:
+#                 j +=1
+#             count +=1
+#             i = j
+#         return count
+
+
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        n = len(s)
+        if n == 0:
+            return 0
+        
+        dp = [[0] * n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = 1
+        
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = dp[i + 1][j] + 1
+                for k in range(i + 1, j + 1):
+                    if s[k] == s[i]:
+                        dp[i][j] = min(dp[i][j], dp[i + 1][k - 1] + dp[k][j])
+        
+        return dp[0][n - 1]
