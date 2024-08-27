57993444b056828128ec465f77c4c9a90b2edba1
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-08-27 23:45:35 +0530

    Time: 40 ms (18.71%), Space: 16.4 MB (95.22%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -5,20 +5,38 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # result =[]
+        
+        # def traverse(node):
+        #     if node:
+        #         # Traverse the left subtree
+        #         traverse(node.left)
+                
+        #         # Traverse the right subtree
+        #         traverse(node.right)
+                
+        #         # Visit the root node (append its value to the result list)
+        #         result.append(node.val)
+        
+        # traverse(root)
+        # return result
+        # if not root:
+        #     return 
+        # else:
+        #     self.root = root.left
+        #     self.root = root.right
+        # result.append(root.val)
+        # return result
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        
-        def traverse(node):
-            if node:
-                # Traverse the left subtree
-                traverse(node.left)
-                
-                # Traverse the right subtree
-                traverse(node.right)
-                
-                # Visit the root node (append its value to the result list)
-                result.append(node.val)
-        
-        traverse(root)
+        self.postorder(root, result)
         return result
+
+    def postorder(self, node, result):
+        if node:
+            self.postorder(node.left, result)
+            self.postorder(node.right, result)
+            result.append(node.val)
