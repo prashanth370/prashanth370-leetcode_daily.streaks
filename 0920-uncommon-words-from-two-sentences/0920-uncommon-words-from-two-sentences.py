3931fdfd56deecc08d02b09458c1c23ce8cbc4c2
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-09-17 23:20:07 +0530

    Time: 34 ms (69.44%), Space: 16.6 MB (44.21%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -1,3 +1,16 @@
+# class Solution:
+#     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+#         word1 = s1.split()
+#         word2 = s1.split()
+#         word_count = {}
+
+#         for w in word1:
+#             word_count[w] = word_count.get(w,0) + 1 
+#         for w in word2:
+#             word_count[w] = word_count.get(w,0) + 1 
+#         unique_w = [w for w in word_count if word_count[w] == 1]
+#         return unique_w
+
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
         # Split the sentences into words
