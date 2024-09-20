d5529ecd0b2f500a25a9181dda0bc9e701dd00de
Banoth Prashanth <85430813+prashanth370@users.noreply.github.com>
2024-09-20 23:03:25 +0530

    Time: 701 ms (19.3%), Space: 16.9 MB (85.39%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,6 +1,25 @@
+# class Solution:
+#     def shortestPalindrome(self, s: str) -> str:
+#         r = s[::-1]
+#         for i in range(len(s) + 1):
+#             if s.startswith(r[i:]):
+#                 return r[:i] + s
+
+
+
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        r = s[::-1]
-        for i in range(len(s) + 1):
-            if s.startswith(r[i:]):
-                return r[:i] + s
+        # l, r = 0, len(s) - 1
+        # a a c e c a a a
+        if s == s[::-1]:
+            return s
+
+        added = ""
+
+        for i in range(len(s) - 1, -1, -1):
+            added += s[i]
+            val = added + s
+            if added + s == val[::-1]:
+                return added + s
+                break
+        return s + s[::-1]
